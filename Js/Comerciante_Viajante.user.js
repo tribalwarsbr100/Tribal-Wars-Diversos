3eed344bb4559083222c17e13e5b775d28afd464
TW 100 <tribalwarsbr100@gmail.com>
2019-05-19 21:50:54 -0300

    Create Comerciante_Viajante.user.js

---
+++ Js/Comerciante_Viajante.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name                Evento Comerciante Viajante
+// @namespace           @@marcosvinicius.santosmarques
+// @icon                https://i.imgur.com/7WgHTT8.gif
+// @website             https://tribalwarsbr100.wixsite.com/tw100
+// @email               tribalwarsbr100@gmail.com
+// @description 	    Script para a o evento comerciante viajante ou comerciante ambulante do game tribalwars
+// @author		        Marcos v.s Marques
+// @include             http*://*.*screen=place&try=confirm*
+// @version     	    0.0.1
+// @copyright           2018, Tribalwarsbr100 (https://openuserjs.org//users/Tribalwarsbr100)
+// @version             0.0.1
+// @license             AGPL-3.0-or-later
+// @grant               GM_getResourceText
+// @grant               GM_addStyle
+// @grant               GM_getValue
+// @grant               unsafeWindow
+// @require             https://pastebin.com/raw.php?i=gtdJNDci
+// ==/UserScript==
+
+/*
+
+Saberás que não te amo e que te amo
+posto que de dois modos é a vida,
+a palavra é uma asa do silêncio,
+o fogo tem uma metade de frio.
+
+Eu te amo para começar a amar-te,
+para recomeçar o infinito
+e para não deixar de amar-te nunca:
+por isso não te amo ainda.
+
+Te amo e não te amo como se tivesse
+em minhas mãos as chaves da fortuna
+e um incerto destino desafortunado.
+
+Meu amor tem duas vidas para amar-te.
+Por isso te amo quando não te amo
+e por isso te amo quando te amo.
+
+
+
+*/
