389b4f2a1eec2f15fa3ca7d6a86d29c7cf36bdcb
TW 100 <tribalwarsbr100@gmail.com>
2019-10-09 11:36:44 -0300

    Create Recaptcha.user.js

---
+++ Js/Recaptcha.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name                Recaptcha Resolve
+// @namespace           @@marcosvinicius.santosmarques
+// @icon                https://i.imgur.com/7WgHTT8.gif
+// @website             https://tribalwarsbr100.wixsite.com/tw100
+// @email               tribalwarsbr100@gmail.com
+// @description 	    Solução para Resolver Recaptcha Automatico
+// @author		        Marcos v.s Marques
+// @include             http*://*.*game.php*
+// @version             0.0.1
+// @copyright           2018, Tribalwarsbr100 (https://openuserjs.org//users/Tribalwarsbr100)
+// @version             0.0.1
+// @license             AGPL-3.0-or-later
+// @grant               GM_getResourceText
+// @grant               GM_addStyle
+// @grant               GM_getValue
+// @grant               unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(function() { document.getElementsByClassName("recaptcha-checkbox-checkmark")[0].click(); }, 1000);
+})();
